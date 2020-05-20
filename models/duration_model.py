0fbe1de36f71ddff2d75b7a7be02fbc32e488e40
Fred-Eric Kirsi <fred.eric.kirsi@gmail.com>
2020-05-21 00:47:11 +0300

    more cleanup - clean models

--- models/duration_model.py
+++ models/duration_model.py
@@ -1,7 +1,7 @@
 from dependencies import *
 
 
-class MultyContextAttention(nn.Module):
+class DurationNetwork(nn.Module):
     def __init__(self, embedding_size, embedding_audio_size, hidden_size, vocab_size, device, dropout=0.1):
         super().__init__()
         self.vocab_size = vocab_size
@@ -103,62 +103,3 @@ class MultyContextAttention(nn.Module):
         current_duration_scalers = total_duration / (current_dur)
         output = (output.squeeze(2) * current_duration_scalers.unsqueeze(1)).unsqueeze(2)
         return output
-
-
-if __name__ == '__main__':
-    duration_combined_model = MultyContextAttention(KNOWN_LABELS_COUNT, INPUT_SIZE, 256, 1, device).to(device).eval()
-    evaluate(duration_combined_model, train_eval_dataset.batch(64), loss_function=MaskedLoss(),
-             train_function=duration_trainer)
-    show_duration_og(duration_combined_model, test_dataset, sample_size=2000)
-    torch.cuda.empty_cache()
-
-    #  Evaluation on train - avg_loss: 0.0064 count:58 Total loss:0.3695
-    """
-    =====  AFTER SCALING
-    [duration] DIFF abs mean: 19.59ms (0.00) min:0.00ms max:1616.81ms
-        20.4%	 < 5ms		39.1%	 < 10ms	
-        54.5%	 < 15ms		66.5%	 < 20ms	
-        75.4%	 < 25ms		81.9%	 < 30ms	
-        86.4%	 < 35ms		89.8%	 < 40ms	
-        92.1%	 < 45ms		93.7%	 < 50ms	
-        95.0%	 < 55ms		96.0%	 < 60ms	
-        96.7%	 < 65ms		97.2%	 < 70ms	
-        97.7%	 < 75ms		98.0%	 < 80ms	
-        98.3%	 < 85ms		98.5%	 < 90ms	
-        98.7%	 < 95ms		98.8%	 < 100ms	
-        98.9%	 < 105ms		100.0%	 < 9999ms	
-    20.37% 39.06% 54.51% 66.53% 75.40% 81.89% 86.43% 89.76% 92.07% 93.74% 95.00% 96.00% 96.69% 97.24% 97.65% 97.98% 98.27% 98.47% 98.67% 98.82% 
-    [position] DIFF abs mean: 71.81ms (-10.80) min:0.00ms max:1616.81ms
-        8.1%	 < 5ms		13.3%	 < 10ms	
-        18.5%	 < 15ms		23.4%	 < 20ms	
-        28.5%	 < 25ms		33.1%	 < 30ms	
-        37.7%	 < 35ms		42.1%	 < 40ms	
-        46.2%	 < 45ms		50.0%	 < 50ms	
-        53.7%	 < 55ms		57.1%	 < 60ms	
-        60.2%	 < 65ms		63.1%	 < 70ms	
-        65.8%	 < 75ms		68.3%	 < 80ms	
-        70.7%	 < 85ms		73.0%	 < 90ms	
-        75.2%	 < 95ms		77.0%	 < 100ms	
-        78.8%	 < 105ms		100.0%	 < 9999ms	
-    8.06% 13.26% 18.49% 23.43% 28.47% 33.12% 37.68% 42.06% 46.24% 50.02% 53.69% 57.05% 60.17% 63.12% 65.76% 68.33% 70.73% 73.04% 75.16% 76.99%
-    """
-
-    # loss_function = MaskedSoftL1(5)
-    # loss_function = MaskedMSE()
-    loss_function = MaskedLoss()
-    # loss_function = DivMaskedMSE(5)
-
-    train_batch_size = 64
-    if WIN_STEP < 0.010:
-        train_batch_size = 16
-
-    train(duration_combined_model, 4, train_dataset.batch(train_batch_size), [train_eval_dataset.batch(64)],
-          loss_function=loss_function, train_function=duration_trainer, lr_decay=0.98, lr=0.000015, weight_decay=1e-05)
-
-    # export_model(duration_combined_model, "/content/drive/My Drive/dataset/multy-attention-duration-normalized-3-[10-25].pth")
-    show_duration_og(duration_combined_model, test_dataset, sample_size=2000)
-    duration_combined_model.eval()
-    torch.cuda.empty_cache()
-
-    # draw_duration(duration_combined_model, test_dataset, 36)
-    # export_model(duration_combined_model, "/content/drive/My Drive/dataset/duration-final-[5-25].pth")
